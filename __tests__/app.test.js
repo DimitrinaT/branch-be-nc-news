98b248bea57fef19f28c608a0fc9e554c188f3d1
DimitrinaT <dimitrina22tancheva@gmail.com>
2023-01-17 23:24:17 +0000

    working on ticket 3

---
+++ __tests__/app.test.js
@@ -0,0 +1,32 @@
+const db = require("../db/connection");
+const request = require("supertest");
+const seed = require("../db/seeds/seed.js");
+const app = require("../app");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("/api/topics", () => {
+  test.only("test endpoint replies with status 200 and returns an array of topic objects ", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.topics).toEqual(testData.topicData);
+
+        response.body.topics.forEach((topic) => {
+          expect(topic).toHaveProperty("description");
+          expect(topic).toHaveProperty("slug");
+        });
+      });
+  });
+  test("test endpoint replies with status 404 when path is mispelt", () => {
+    return request(app).get("/api/topiks").expect(404);
+  });
+});
